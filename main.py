3043a9da5799a2bf1b724b44d8ffa43af84d2f16
Bhupesh <bhupeshchandrawanshi@gmail.com>
2021-03-19 13:05:10 +0000

    Updated the result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 # Initialise the pygame module
 pygame.init()
@@ -24,6 +25,8 @@ for i in range(26):
 
 # Fonts
 LETTER_FONT = pygame.font.SysFont('comicsans', 30)
+WORD_FONT = pygame.font.SysFont('comicsans', 60)
+TITLE_FONT = pygame.font.SysFont('comicsans', 60)
 
 # Load images
 images = []
@@ -33,11 +36,28 @@ for i in range(7):
 
 # Game variables
 hangman_stats = 0
+words = ["CODING","GITHUB","REPLIT", "PYTHON", "HANGMAN","COMPUTER", "KEYBOARD", "MOBILE", "DEVELOPER"]
+word = random.choice(words)
+guessed = []
 
 def draw():
    # Update and refresh the display
-
   win.fill(WHITE)
+
+  # Draw Title
+  text = TITLE_FONT.render("DEVELOPER HANGMAN", 1, BLACK)
+  win.blit(text, (WIDTH/2- text.get_width()/2, 20))
+
+  # Draw Word
+  display_word = ""
+  for letter in word:
+    if letter in guessed:
+      display_word += letter + " "
+    else:
+      display_word += "_ "
+
+  text = WORD_FONT.render(display_word, 1 , BLACK)
+  win.blit(text,(400,200))
 
   # Draw Buttons
   for letter in letters:
@@ -47,9 +67,17 @@ def draw():
       text = LETTER_FONT.render(ltr, 1 , BLACK)
       win.blit(text,(x-text.get_width()/2,y-text.get_height()/2))
 
-  win.blit(images[hangman_stats], (100,50))
+  win.blit(images[hangman_stats], (100,100))
   pygame.display.update()
 
+# Display Messsage Function
+def display_message(message):
+  pygame.time.delay(1000)
+  win.fill(WHITE)
+  text = WORD_FONT.render(message, 1, BLACK)
+  win.blit(text, (WIDTH/2 - text.get_width()/2,HEIGHT/2 - text.get_height()/2))
+  pygame.display.update()
+  pygame.time.delay(3000)
 
 
 # Setup game loop 
@@ -59,8 +87,6 @@ run = True
 
 while run:
   clock.tick(FPS)
-
-  draw()
 
   for event in pygame.event.get():
 
@@ -76,5 +102,23 @@ while run:
         dis = math.sqrt((x-m_x)**2+(y-m_y)**2)
         if dis < RADIUS:
           letter[3] = False
+          guessed.append(ltr)
+          if ltr not in word:
+            hangman_stats += 1
+
+    draw()
+  
+  won = True
+  for letter in word:
+    if letter not in guessed:
+      won = False
+      break
+
+  if won:
+    display_message("YOU WON!")
+
+  if hangman_stats == 6:
+    display_message("YOU LOST!")
+    break
 
 pygame.quit()
